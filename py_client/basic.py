cb41e0971389e2f41b78904168e0db86c254016c
VEGA/VNM <vnjeru@vegasoftware.com>
2022-08-30 12:24:57 +0300

    add api urls

--- py_client/basic.py
+++ py_client/basic.py
@@ -2,8 +2,8 @@ import requests
 
 # endpoint = "https://httpbin.org/"
 # endpoint = "https://httpbin.org/anything"
-endpoint = "http://localhost:8000"
+endpoint = "http://localhost:8000/api"
 
 
 response = requests.get(endpoint)
-print(response.status_code)
+print(response.json())
