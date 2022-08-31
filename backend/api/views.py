c0d01226ae16badf614183bec179cb8d86711e2e
VEGA/VNM <vnjeru@vegasoftware.com>
2022-08-31 13:47:58 +0300

    convert to django restframework view (api_view)

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,14 +1,19 @@
-from django.http import JsonResponse
 from django.forms.models import model_to_dict
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
 from api.models import Product
 
-
+@api_view(["GET"])
 def api_home (request, *args, **kwargs):
+    """ This is now a Django RestFramework View """
+
+    # if request.method != "POST":
+    #     return Response({"detail": "GET not allowed"}, status=405)
+
     model_data = Product.objects.all().first()
     data = {}
     if model_data:
-        # data = model_to_dict(model_data)
-        data = model_to_dict(model_data,fields=['id', 'name']) #specify only wanted fields
-    
-    return JsonResponse(data)
+        data = model_to_dict(model_data,fields=['id', 'name'])
+
+    return Response(data)
