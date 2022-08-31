c89e5bf9433e68387fe9a32b1ef4236cb792caf2
VEGA/VNM <vnjeru@vegasoftware.com>
2022-08-31 13:33:26 +0300

    model instance to dictionary - cleaner way

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,23 +1,14 @@
-import json
 from django.http import JsonResponse
+from django.forms.models import model_to_dict
 
 from api.models import Product
 
 
 def api_home (request, *args, **kwargs):
     model_data = Product.objects.all().first()
-    
     data = {}
-
     if model_data:
-        data['id'] = model_data.id
-        data['imageUrl'] = model_data.imageUrl
-        data['name'] = model_data.name
-        data['price'] = model_data.price
-        data['rating'] = model_data.rating
-        data['producer'] = model_data.producer
-        data['unit'] = model_data.unit
-        data['category'] = model_data.category
-        data['isFeatured'] = model_data.isFeatured
+        # data = model_to_dict(model_data)
+        data = model_to_dict(model_data,fields=['id', 'name']) #specify only wanted fields
     
     return JsonResponse(data)
